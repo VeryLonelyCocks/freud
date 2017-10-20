5b3fa8e0432b05a8a21926312135ec6ca3779631
Taly <vitalik7tv@yandex.ru>
2017-10-21 01:22:12 +0300

    listener

--- freud/__init__.py
+++ freud/__init__.py
@@ -1,16 +1,25 @@
 from aiohttp import web
 import asyncio
-import os
 
 PORT = 3000
 
-async def main_page(request):
+async def vk_callback(request):
     """
     Web App function for processing callback
     """
-    return web.Response(text='Freud is here')
+    try:
+        data = await request.json()
+        print(data)
+    except Exception as e:
+        print("Message process error: [%s]" % e)
+
+    return web.Response(text='Ok')
+
+async def main_page(request):
+    return web.Response(text='Shive is alive')
 
 loop = asyncio.get_event_loop()
 app = web.Application(loop=loop)
 app.router.add_get('/', main_page)
+app.router.add_post('/vk', vk_callback)
 web.run_app(app, port=PORT)
